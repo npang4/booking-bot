fcdc366d44160cf6f0d83e1bf167d7ef18107238
Nelson Pang <nelsoncpang@gmail.com>
2022-12-18 23:59:28 -0800

    uploaded new files

---
+++ booking/booking.py
@@ -0,0 +1,14 @@
+import booking.constants as const
+import os
+from selenium import webdriver
+
+
+class Booking(webdriver.Chrome):
+    def __init__(self, driver_path="C:\SeleniumDrivers"):
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Booking, self).__init__()
+    
+    def land_first_page(self):
+        self.get(const.BASE_URL)
+        
